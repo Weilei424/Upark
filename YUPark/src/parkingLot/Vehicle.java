37f302961c3d42e9684b9c77e59b0748c920eb34
Zhenxu Wang <mason123wp@gmail.com>
2023-01-28 11:14:08 -0500

    implemented constructor and static factory method

--- YUPark/src/parkingLot/Vehicle.java
+++ YUPark/src/parkingLot/Vehicle.java
@@ -1,5 +1,27 @@
 package parkingLot;
 
 public class Vehicle {
-
+	protected String make;
+	protected String model;
+	protected String color;
+	protected int year;
+	protected String plate;
+	
+	private Vehicle() {
+		this("", "", "", 0, "");
+	}
+	
+	private Vehicle(String make, String model, String color, int year, String plate) {
+		this.make = make;
+		this.model = model;
+		this.color = color;
+		this.year = year;
+		this.plate = plate;
+	}
+	
+	public Vehicle getInstanceOf(String make, String model, String color, int year, String plate) {
+		Vehicle v = new Vehicle(make, model, color, year, plate);
+		return v;
+	}
+	
 }
