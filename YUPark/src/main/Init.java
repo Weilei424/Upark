0f512f1fbb9eae18e64dec1686466f9133282eef
Zhenxu Wang <mason123wp@gmail.com>
2023-01-28 14:34:58 -0500

    initializing lots in main package

--- YUPark/src/main/Init.java
+++ YUPark/src/main/Init.java
@@ -1,10 +1,85 @@
 package main;
 
+import java.util.ArrayList;
+
+import parkingLot.Lot;
+
 public class Init {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<Lot> lots = new ArrayList<Lot>();
+		Lot apg = new Lot("g");
+		apg.setLotNum(80);
+		apg.setCap(400);
+		lots.add(apg);
+		
+		Lot spg = new Lot("g");
+		spg.setLotNum(84);
+		spg.setCap(600);
+		lots.add(spg);
+		
+		Lot ypg = new Lot("g");
+		ypg.setLotNum(72);
+		ypg.setCap(400);
+		lots.add(ypg);
+		
+		Lot atk = new Lot("s");
+		atk.setLotNum(83);
+		atk.setCap(25);
+		lots.add(atk);
+		
+		Lot cal = new Lot("s");
+		cal.setLotNum(107);
+		cal.setCap(8);
+		lots.add(cal);
+		
+		Lot fre = new Lot("s");
+		fre.setLotNum(66);
+		fre.setCap(175);
+		lots.add(fre);
+		
+		Lot lum = new Lot("s");
+		lum.setLotNum(73);
+		lum.setCap(25);
+		lots.add(lum);
+		
+		Lot nel = new Lot("s");
+		nel.setLotNum(82);
+		nel.setCap(60);
+		lots.add(nel);
+		
+		Lot apg = new Lot("APG");
+		apg.setLotNum(80);
+		apg.setCap(400);
+		lots.add(apg);
+		
+		Lot spg = new Lot("SPG");
+		spg.setLotNum(84);
+		spg.setCap(600);
+		lots.add(spg);
+		
+		Lot apg = new Lot("APG");
+		apg.setLotNum(80);
+		apg.setCap(400);
+		lots.add(apg);
+		
+		Lot spg = new Lot("SPG");
+		spg.setLotNum(84);
+		spg.setCap(600);
+		lots.add(spg);
+		
+		Lot apg = new Lot("APG");
+		apg.setLotNum(80);
+		apg.setCap(400);
+		lots.add(apg);
+		
+		Lot spg = new Lot("SPG");
+		spg.setLotNum(84);
+		spg.setCap(600);
+		lots.add(spg);
+		
+		
+		
 	}
 
 }
--- YUPark/src/parkingLot/Lot.java
+++ YUPark/src/parkingLot/Lot.java
@@ -1,9 +1,38 @@
 package parkingLot;
 
 public class Lot {
-	protected String lotName;
+	protected String lotType;
 	protected int lotNum;
 	protected int cap;
-	protected int counter;
+	public int counter;
 	
+	public Lot() {
+		
+	}
+	
+	public Lot(String lotType) {
+		this.lotType = lotType;
+		this.cap = 0;
+		this.counter = 0;
+	}
+	
+	public String getLotType() {
+		return lotType;
+	}
+
+	public int getLotNum() {
+		return lotNum;
+	}
+	
+	public void setLotNum(int lotNum) {
+		this.lotNum = lotNum;
+	}
+	
+	public int getCap() {
+		return cap;
+	}	
+	
+	public void setCap(int cap) {
+		this.cap = cap;
+	}
 }
