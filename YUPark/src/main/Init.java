8f8122db018a9200f020fadcb532da5ab4704e24
Zhenxu Wang <mason123wp@gmail.com>
2023-01-28 16:14:02 -0500

    initialized all lots in init

--- YUPark/src/main/Init.java
+++ YUPark/src/main/Init.java
@@ -70,13 +70,88 @@ public class Init {
 		
 		Lot tre = new Lot("s");
 		tre.setLotNum(77);
-		tre.setCap();
+		tre.setCap(6);
 		lots.add(tre);
 		
-		Lot spg = new Lot("SPG");
-		spg.setLotNum(84);
-		spg.setCap(600);
-		lots.add(spg);
+		Lot tr = new Lot("s");
+		tr.setLotNum(79);
+		tr.setCap(30);
+		lots.add(tr);
+		
+		Lot vl = new Lot("s");
+		vl.setLotNum(71);
+		vl.setCap(50);
+		lots.add(vl);
+		
+		Lot ar = new Lot("s");
+		ar.setLotNum(67);
+		ar.setCap(80);
+		lots.add(ar);
+		
+		Lot cl = new Lot("s");
+		cl.setLotNum(97);
+		cl.setCap(30);
+		lots.add(cl);
+		
+		Lot frw = new Lot("s");
+		frw.setLotNum(65);
+		frw.setCap(110);
+		lots.add(frw);
+		
+		Lot ha = new Lot("s");
+		ha.setLotNum(88);
+		ha.setCap(76);
+		lots.add(ha);
+		
+		Lot kl = new Lot("s");
+		kl.setLotNum(87);
+		kl.setCap(5);
+		lots.add(kl);
+		
+		Lot ln = new Lot("s");
+		ln.setLotNum(74);
+		ln.setCap(8);
+		lots.add(ln);
+		
+		Lot pc = new Lot("s");
+		pc.setLotNum(91);
+		pc.setCap(30);
+		lots.add(pc);
+		
+		Lot nr = new Lot("s");
+		nr.setLotNum(82);
+		nr.setCap(40);
+		lots.add(nr);
+		
+		Lot prn = new Lot("s");
+		prn.setLotNum(85);
+		prn.setCap(16);
+		lots.add(prn);
+		
+		Lot pre = new Lot("s");
+		pre.setLotNum(68);
+		pre.setCap(44);
+		lots.add(pre);
+		
+		Lot sh = new Lot("s");
+		sh.setLotNum(63);
+		sh.setCap(80);
+		lots.add(sh);
+		
+		Lot tpr = new Lot("s");
+		tpr.setLotNum(86);
+		tpr.setCap(70);
+		lots.add(tpr);
+		
+		Lot wob = new Lot("s");
+		wob.setLotNum(78);
+		wob.setCap(6);
+		lots.add(wob);
+		
+		Lot yb = new Lot("s");
+		yb.setLotNum(70);
+		yb.setCap(140);
+		lots.add(yb);
 		
 		
 		
